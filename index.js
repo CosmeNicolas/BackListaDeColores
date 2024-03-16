bb669c7dd3379852e594e0b842be0ee6f84bb51a
CosmeNicolas <nicomas7590@gmail.com>
2024-03-16 20:56:50 -0300

    agregue los middleware de express

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import morgan from 'morgan'
+
+
+
+
 const app = express()
-
-
-/* console.log('desde el index.js') */
-
 //1 configurar el puerto 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
@@ -16,6 +16,8 @@ app.listen(app.get('port'), ()=>{
 //2 configurar los middlewares
 app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 //3 configurar las rutas 
 app.get('/',(req, res)=>{
     //acá va la logica de la peticion
