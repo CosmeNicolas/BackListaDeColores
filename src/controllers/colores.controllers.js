efcce204d3221fb62890d2ab02efa762c2dde145
CosmeNicolas <nicomas7590@gmail.com>
2024-03-19 19:47:49 -0300

    agregue la funcion y la ruta de listar colores

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -1,6 +1,11 @@
 import Color from "../database/models/color.js"
 
-export const listarColores = (req, res)=>{
-    console.log('Listando colores')
-    res.send('Enviando colores')
+export const listarColores = async(req, res)=>{
+    try {
+        const colores = await Color.find()
+        res.status(200).json(colores)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({mensaje:'Error al buscar el color'})
+    }
 }
