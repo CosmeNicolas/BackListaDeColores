08bae6c05f78ddd6572bc64c7197a2ba6fa96f19
CosmeNicolas <nicomas7590@gmail.com>
2024-03-19 20:31:40 -0300

    agregue la ruta y la funcion de editar color

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -33,3 +33,17 @@ export const obtenerColor = async(req,res)=>{
         res.status(500).json({mensaje:'No se encontro el color con su Id'})
     }
 }
+
+export const editarColor = async(req, res)=>{
+    try {
+        const buscarColor = await Color.findById(req.params.id)
+        if(!buscarColor){
+            return res.status(404).json({mensaje:'No se pudo encontrar el color para editar'})
+        }
+        await Color.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({mensaje:'Color editado'})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({mensaje:'No se pudo editar el color'})
+    }
+}
